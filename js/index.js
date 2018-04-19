ced70670a267fb4471c468e9dc2bc7032fc7c2f7
Allan Adimoolah <allanstarr023@yahoo.com>
2018-04-18 23:00:57 -0400

    One Signal - Push Notifications
    One Signal - Push Notifications being tested

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('deviceready', .onDeviceReady, false);
 
-function onDeviceReady(){
+/*function onDeviceReady(){
     document.addEventListener("backbutton", function(e){
        if($.mobile.activePage.is('#homepage')){
            e.preventDefault();
@@ -10,4 +10,18 @@ function onDeviceReady(){
            navigator.app.backHistory();
        }
     }, false);	
-}
+}*/
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("7c757bff-58d9-43ef-bbd2-4a8ac00e04a2")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
