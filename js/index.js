dd8639b29f38572aa95f9cfc07b187d3c374b5cb
allanstarr023 <37152409+allanstarr023@users.noreply.github.com>
2018-07-13 12:36:01 -0400

    OneSignal Testing
    Testing One Signal here

--- js/index.js
+++ js/index.js
@@ -12,9 +12,28 @@ document.addEventListener('deviceready', .onDeviceReady, false);
     }, false);	
 }*/
 
+/* 
 document.addEventListener('deviceready', function () {
   // Enable to debug issues.
   // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("7c757bff-58d9-43ef-bbd2-4a8ac00e04a2")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
+*/
+
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   
   var notificationOpenedCallback = function(jsonData) {
     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
